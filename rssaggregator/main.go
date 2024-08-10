2189682de443cff61ec97fe7b94403bcadbacb97
Himanshu Rai <himanshuraimau9@gmail.com>
2024-08-10 22:59:18 +0530

    add chi

--- rssaggregator/main.go
+++ rssaggregator/main.go
@@ -3,16 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-	godotenv.Load()
+	godotenv.Load(".env")
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT environment variable not set")
+	}
+	router := chi.NewRouter()
+	srv := &http.Server {
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+    err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
 	}
 	fmt.Printf("Listening on port %s\n", portString)
 }
