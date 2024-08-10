91f99f39a091d2158745b3a01a8cf29aa41f6768
Himanshu Rai <himanshuraimau9@gmail.com>
2024-08-10 22:38:51 +0530

    ADD godotenv

--- rssaggregator/go.mod
+++ rssaggregator/go.mod
@@ -1,3 +1,5 @@
 module github.com/himanshuraimau/backend_projects/rssaggregator
 
 go 1.22.4
+
+require github.com/joho/godotenv v1.5.1
--- rssaggregator/main.go
+++ rssaggregator/main.go
@@ -1,8 +1,18 @@
 package main
 
-
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+	fmt.Printf("Listening on port %s\n", portString)
 }
