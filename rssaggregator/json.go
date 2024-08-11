24f86802375847741d5f220868c53d1d72648223
Himanshu Rai <himanshuraimau9@gmail.com>
2024-08-12 01:12:52 +0530

    add boiler

--- rssaggregator/json.go
+++ rssaggregator/json.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"http"
 	"encoding/json"
-	
+	"log"
+	"net/http"
 )
 
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	if code>499{
+		log.Println("Responding with 5XX error: ", message)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{Error: message})
+}
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, err := json.Marshal(payload)
--- rssaggregator/main.go
+++ rssaggregator/main.go
@@ -27,6 +27,12 @@ func main() {
 		AllowCredentials: false,
 		MaxAge: 300, // Maximum value not ignored by any of major browsers
 	}))
+
+    v1Router := chi.NewRouter()
+	v1Router.Get("/health", handlerReadiness)
+	v1Router.Get("/error", handleErr)
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server {
 		Handler: router,
 		Addr:    ":" + portString,
