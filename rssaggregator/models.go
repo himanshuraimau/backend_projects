d8af01fd8a8dc70441e5566c1e943772b7bd97bc
Himanshu Rai <himanshuraimau9@gmail.com>
2024-08-22 22:58:17 +0530

    add models

---
+++ rssaggregator/models.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/himanshuraimau/backend_projects/rssaggregator/internal/database"
+)
+
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func databaseUserToUser(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID,
+		Name:      dbUser.Name,
+		CreatedAt: dbUser.CreatedAt.Time,
+		UpdatedAt: dbUser.UpdatedAt.Time,
+	}
+}
